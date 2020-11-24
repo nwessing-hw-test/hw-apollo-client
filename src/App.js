e397492b25f4aaedca591e1521e93782db3d2ff2
nwessing-hw-test <74916317+nwessing-hw-test@users.noreply.github.com>
2020-11-24 09:19:47 -0600

    Use tailwind

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from "react";
 import { ApolloProvider } from "@apollo/client";
 import apolloClient from "./apolloClient";
 
-import "./styles.css";
-
 export default function App() {
   return (
     <ApolloProvider client={apolloClient}>
-      <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+      <div className="container mx-auto">
+        <h1 className="text-2xl mb-4">Link Shrinker</h1>
+      </div>
+      <div class="bg-blue-800 p-8">
+        <div class="container mx-auto">
+          <form className="flex justify-center">
+            <input className="p-2" name="url" />
+            <input
+              className="bg-blue-400 ml-4 px-4 text-white"
+              type="submit"
+              value="Shorten"
+            />
+          </form>
+        </div>
       </div>
     </ApolloProvider>
   );
